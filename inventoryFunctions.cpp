38f7a382247bfae0f9b8d8cb8c03fba88cda7d59
DaniHB // LemonpieGBS <142750834+LemonpieGBS@users.noreply.github.com>
2024-06-17 22:05:36 -0600

    added Inventory Functions development

---
+++ inventoryFunctions.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <chrono>
+
+#define sleep(x) std::this_thread::sleep_for(std::chrono::seconds(x))
+#define MAX_INVENTORY_LENGTH 100
+
+struct InventoryItems {
+
+    std::string id;
+    std::string name;
+    std::string provider;
+    int quantity_left;
+    float price;
+
+};
+
+std::string GenerateID();
+void addToInventory(InventoryItems inventario[], int &inventory_size);
+void removeFromInventory(InventoryItems inventario[], int &inventory_size);
+void showInventory(InventoryItems inventario[], int inventory_size);
+
+int main() {
+
+    int inventory_items = 0;
+    InventoryItems inventario[MAX_INVENTORY_LENGTH];
+
+    while(true) {
+
+        system("cls");
+
+        int input;
+        std::cout<<"Menu de Inventario";
+        std::cout<<"\nEliga lo que quiera hacer:\n";
+
+        std::cout<<"\n 1.Añadir objeto a inventario";
+        std::cout<<"\n 2.Remover objeto de inventario";
+        std::cout<<"\n 3.Editar objeto de inventario";
+        std::cout<<"\n 4.Mostrar inventario";
+        std::cout<<"\n 5.<- Salir";
+        std::cout<<"\n\n#:";
+
+        std::cin>>input;
+
+        switch(input) {
+            case(1): addToInventory(inventario,inventory_items); break;
+            case(2): break;
+            case(3): break;
+            case(4): showInventory(inventario,inventory_items); system("pause"); break;
+            case(5): return 0; break;
+        }
+    }
+}
+
+std::string GenerateID() {
+    char allowed_characters[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G'
+    ,'H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','Y','X','Z'};
+
+    int charset_size = sizeof(allowed_characters) / sizeof(allowed_characters[0]);
+
+    std::string ID;
+    int ID_SIZE = 8;
+
+    for(int i = 0; i < ID_SIZE; i++) {
+        ID += allowed_characters[rand() % charset_size];
+    }
+
+    return ID;
+}
+
+void addToInventory(InventoryItems inventario[], int &inventory_size) {
+
+    system("cls");
+    InventoryItems new_item;
+
+    std::cin.ignore();
+    
+    std::cout<<"Menu de adición al inventario";
+    std::cout<<"\n\nInserte el nombre del producto: ";
+    getline(std::cin,new_item.name);
+
+    std::cout<<"\nInserte el proveedor del producto: ";
+    getline(std::cin,new_item.provider);
+
+    std::cout<<"\nInserte la cantidad de unidades: ";
+    std::cin>>new_item.quantity_left;
+
+    std::cout<<"\nInserte el precio del producto: ";
+    std::cin>>new_item.price;
+
+    new_item.id = GenerateID();
+
+    inventario[inventory_size] = new_item;
+    inventory_size++;
+
+    std::cout<<"\nObjeto añadido con exito!";
+    sleep(2);
+}
+
+void removeFromInventory(InventoryItems inventario[], int &inventory_size) {
+
+    showInventory(inventario,inventory_size);
+    if(inventory_size <= 0) { sleep(2); return; }
+
+    std::string ID_input;
+    std::cout<<"\nInserte el ID del producto a eliminar: ";
+    std::cin>>ID_input;
+
+    int itemFound = -1;
+    for(int i = 0; i < inventory_size; i++) {
+        if(ID_input == inventario[i].id) { itemFound = i; break; }
+    }
+
+    if(itemFound == -1) {
+
+    }
+}
+
+void showInventory(InventoryItems inventario[], int inventory_size) {
+
+    system("cls");
+
+    if(inventory_size == 0) { std::cout<<"No hay objetos en el inventario!"; return; }
+
+    for(int i = 0; i < inventory_size; i++) {
+        std::cout<<"- ID: "<<inventario[i].id<<", Nombre: "<<inventario[i].name;
+        std::cout<<"\n  Proveedor: "<<inventario[i].provider<<", Unidades: "<<inventario[i].quantity_left;
+        std::cout<<"\n  Precio: "<<inventario[i].price<<"\n";
+    }
+
+}
